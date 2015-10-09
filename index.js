bf46d32577606dae0bad40b184dde78f253fbb60
zhongchiyu <zhongchiyu@gmail.com>
2015-10-09 11:12:52 +0800

    Fix missing some fields

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 var os = require('os');
+var ip = require('ip');
 var util = require('util');
 var winston = require('winston');
 var Client = require('scribe').Scribe;
@@ -29,6 +30,7 @@ var Scribe = winston.transports.Scribe = function (options) {
     this.app = options.app || 'winston';
     this.category = options.category;
     this.host = os.hostname();
+    this.ip = ip.address();
     this.client = new Client(options.host, options.port, {autoReconnect: true});
     this.client.open(function(err) {
         if (err) {
@@ -53,6 +55,35 @@ Scribe.LEVELS_MAP = {
     error: 'ERROR',
 };
 
+Scribe.prototype.formatTimestamp = function(t) {
+    var month = "" + (t.getMonth() + 1);
+    if (month.length === 1) {
+        month = "0" + month;
+    }
+
+    var days = "" + t.getDate();
+    if (days.length === 1) {
+        days = "0" + days;
+    }
+
+    var hours = "" + t.getHours();
+    if (hours.length === 1) {
+        hours = "0" + hours;
+    }
+
+    var minutes = "" + t.getMinutes();
+    if (minutes.length === 1) {
+        minutes = "0" + minutes;
+    }
+
+    var seconds = "" + t.getSeconds();
+    if (seconds.length === 1) {
+        seconds = "0" + seconds;
+    }
+
+    return t.getFullYear() + "-" + month + "-" + days + " " + hours + ":" + minutes + ":" + seconds;
+};
+
 /**
  * Log format
  *
@@ -73,15 +104,17 @@ Scribe.prototype.log = function (level, message, meta, callback) {
         message = message + ': ' + meta.error.message + ' -- ' + meta.error.stack;
     }
     level = Scribe.LEVELS_MAP[level] || this.level;
+    var datetime = this.formatTimestamp(new Date());
     var version = this.version;
     var app = this.app;
     var host = this.host;
+    var ip = this.ip;
     var timestamp = Math.floor(Date.now() / 1000);
     // default log type is system log
     var type = meta.type || 'system';
     var requestID = meta.requestID || 'default';
 
-    var log = util.format('version=%s app=%s host=%s level=%s timestamp=%s type=%s requestID=%s message=%s\n', version, app, host, level, timestamp, type, requestID, message);
+    var log = util.format('%s %s %s %s version=%s app=%s host=%s level=%s timestamp=%s type=%s requestID=%s message=%s\n', datetime, level, host, ip, version, app, host, level, timestamp, type, requestID, message);
     this.client.send(this.category, log);
     callback(null, true);
 };
